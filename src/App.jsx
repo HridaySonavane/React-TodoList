585448b3269d9851f0bce984610c82deeb9aaa76
HridaySonavane <hridays2005@gmail.com>
2024-04-15 15:11:06 +0530

    fixed input tag issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React, { useState } from "react";
 
 function App() {
+  const [first, setfirst] = useState('')
+
+  const handleChange = e =>{
+    setfirst(e.target.value)
+  }
+  
   return (
-    <Navbar/>
-  )
+    <div className="absolute flex justify-center items-center w-screen h-full bg-zinc-900">
+      {/* <FaPlus/> */}
+      <div className="card relative w-[450px] h-[500px] bg-white rounded-md">
+        <div className="text-3xl font-bold mb-10 ml-4 mt-4">ToDo-List</div>
+        <div className="flex relative justify-evenly ">
+          <input
+            className="border border-slate-400 px-2 py-1 w-72 rounded-md"
+            type="text"
+            placeholder="Add your New ToDo"
+            value={first}
+            onChange={handleChange}
+          />
+          <button
+            className="bg-violet-900 text-white px-3 rounded-md"
+            type="submit"
+          >
+            Add a ToDo
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,10 @@ function Navbar() {
     <div className='absolute flex justify-center items-center w-screen h-full bg-zinc-900'>
       {/* <FaPlus/> */}
       <div className="card relative w-[450px] h-[500px] bg-white rounded-md">
-        <div>ToDo-List</div>
-        <div>
+        <div className='text-3xl mb-10 ml-4 mt-4'>ToDo-List</div>
+        <div className='flex relative justify-center border border-black'>
           <input className='border border-slate-400 px-2 py-1 w-72' type="text" placeholder='Add your New ToDo' />
-          <button className='bg-violet-900 text-white h-7' type="submit"> <FaPlus size={22} /> </button>
+          <button className='bg-violet-900 text-white px-2' type="submit">Add a ToDo</button>
         </div>
         
       </div>
