e60524b1c7cd667a690d3c0c79c8d84b9c070a21
HridaySonavane <hridays2005@gmail.com>
2024-04-17 22:07:30 +0530

    normal commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <TodoList/>
-      <Navbar/>
-    </div>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route exact path="/" element={<TodoList />} />
+
+          <Route exact path="about/*" element={<About/>} />
+        </Routes>
+      </div>
+      <Navbar />
+      
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 import { GoHomeFill } from "react-icons/go";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="flex justify-center">
-      <div className='absolute top-6 flex justify-around items-center w-6/12 h-12 rounded-full backdrop-blur-3xl bg-gradient-to-br from-white/70 border border-white font-semibold text-lg '>
-        <a href="">Home</a>
-        <a href="">About</a>
-        <a href="">Bg Gallery</a>
-        <a href="">Contact Us</a>
-        <button className="bg-black text-white px-3 py-1 rounded-full">Sign Up</button>
+      <div className="absolute top-6 flex justify-around items-center w-6/12 h-12 rounded-full backdrop-blur-3xl bg-white border border-white font-semibold text-lg">
+        <Link to="/">Home</Link>
+        <Link to="/about/*">About</Link>
+        <Link to="/">Bg Gallery</Link>
+        <Link to="/">Contact Us</Link>
+        <button className="bg-black text-white px-3 py-1 rounded-full">
+          Sign Up
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
