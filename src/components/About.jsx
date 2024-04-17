482e3a2a047af8c6233f7b2a9282ae1c1c59def9
HridaySonavane <hridays2005@gmail.com>
2024-04-17 22:06:15 +0530

    added an About Us Page to the mix

---
+++ src/components/About.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <div className="absolute top-0 w-screen bg-zinc-900 h-1/2" />
+
+      <div className="w-screen relative top-52 flex flex-col justify-center items-center">
+        <img
+          className="w-96 mb-10 rounded-md"
+          src="https://img.freepik.com/premium-photo/group-people-standing-together-city-generative-ai-image_97070-40854.jpg?w=1060"
+          alt=""
+        />
+
+        <p className="w-3/6 text-justify text-lg font-medium">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia officia
+          culpa quo, fugiat provident modi alias aperiam! Nam incidunt
+          exercitationem praesentium repudiandae a aperiam doloremque quo aut,
+          nisi ipsum molestiae. Obcaecati nesciunt suscipit dolorum saepe? Sed
+          suscipit similique voluptates in aspernatur totam odio perspiciatis.
+          Impedit illo, corporis ex amet vero vel quisquam enim fugit illum,
+          magnam dolorum ea quidem nemo? Expedita minima atque provident illum
+          commodi quibusdam, inventore nobis ut assumenda dicta enim laboriosam
+          ipsam delectus molestias officiis placeat quis at labore saepe
+          consectetur sapiente ea pariatur natus dignissimos. Id. Saepe dicta
+          ducimus, odio corporis asperiores sequi doloribus recusandae
+          praesentium obcaecati necessitatibus, id sapiente quo ea. Hic totam
+          vitae ex deserunt temporibus, deleniti eveniet omnis quibusdam. Quos
+          alias earum soluta. Similique atque dolor incidunt optio vel dolores,
+          aspernatur rerum nesciunt veniam, quidem suscipit quibusdam tenetur
+          architecto qui deserunt sapiente ipsum quas ut? Maiores sunt
+          repudiandae vero tempore, optio blanditiis recusandae. Aliquam nam est
+          saepe consectetur totam repellendus rem eaque error dolorem deserunt
+          eius ex distinctio cum ullam aut accusamus repellat corrupti facere
+          necessitatibus fuga, sequi vel porro ea expedita. Cum! Blanditiis
+          veniam, quod dolor eligendi ea voluptatibus, impedit nihil tempora
+          laborum iure minima numquam pariatur, architecto ut animi? Mollitia
+          sunt voluptatem asperiores omnis. Rerum dolor facere ea, tempora
+          placeat omnis? Excepturi dolorum ducimus expedita fuga amet provident
+          saepe rem qui, impedit iure laudantium debitis. Laboriosam sed unde
+          modi aliquam. Nemo magnam eius tempora? Vero hic necessitatibus
+          commodi odio, amet reprehenderit! Debitis harum consectetur accusamus
+          illum in quod alias qui, inventore fugit sunt ratione dignissimos quis
+          blanditiis magni quidem iste placeat nostrum vel eos numquam veniam
+          non delectus! Sit, enim temporibus. Et nostrum nesciunt inventore
+          repellat labore itaque laudantium, nobis doloremque voluptatum iure
+          incidunt optio cupiditate exercitationem minima, non ullam
+          consequuntur nam! Veritatis voluptates quod laudantium accusamus porro
+          fugiat voluptate iste.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
